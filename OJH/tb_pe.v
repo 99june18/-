8f445a80a5523da82ab0fcb2cde9bdfcf16531d2
jinhyeok <jinhyeok1003@gmail.com>
2022-12-27 16:38:58 +0900

    누산기는 아마 더이상 안건들일듯

--- OJH/tb_pe.v
+++ OJH/tb_pe.v
@@ -1,60 +1,58 @@
-`timescale 1ns/1ns
+/**    `timescale 1ns/1ns
 
-module tb_pe;
+    module tb_pe;
 
-	reg CLK; 
-	reg RST;
+        reg CLK; 
+        reg RST;
 
-	reg [7:0] PE_IN;
-    reg [7:0] PE_FILTER;
- 	
-    wire [7:0] PE_OUT;
-    wire PE_VAILD;
+        reg [7:0] PE_IN;
+        reg [7:0] PE_FILTER;
+        
+        wire [7:0] PE_OUT;
+        wire PE_VAILD;
 
-    integer count1, count2, count3, count4;
+        integer count1, count2, count3, count4;
 
-    pe u_pe(.clk(CLK), .rst(RST), .pe_in(PE_IN), .pe_filter(PE_FILTER), .pe_out(PE_OUT), .pe_vaild(PE_VAILD));
+        pe u_pe(.clk(CLK), .rst(RST), .pe_in(PE_IN), .pe_filter(PE_FILTER), .pe_out(PE_OUT), .pe_vaild(PE_VAILD));
 
-	initial
-	begin
-	    forever
-		begin
-		    #10 CLK = !CLK;
-		end
-	end
-
-    // initial setting
-	initial 
-	begin
-		CLK = 0;
-        RST = 1;
-        PE_IN = 10;
-        PE_FILTER = 10;
-	end
-
-    initial 
-	begin
-		#20 RST = 0;
-        for (count1 = 0; count1 < 9; count1 = count1 + 1) begin
-            #20 PE_IN = 10;
-                PE_FILTER = 10;
-        end
-        for (count2 = 0; count2 < 9; count2 = count2 + 1) begin
-            #20 PE_IN = 5;
-                PE_FILTER = 5;
-        end
-        for (count3 = 0; count3 < 9; count3 = count3 + 1) begin
-            #20 PE_IN = 2;
-                PE_FILTER = 2;
-        end
-        for (count4 = 0; count4 < 9; count4 = count4 + 1) begin
-            #20 PE_IN = 20;
-                PE_FILTER = 20;
+        initial
+        begin
+            forever
+            begin
+                #10 CLK = !CLK;
+            end
         end
 
-        #20 RST = 1;
+        // initial setting
+        initial 
+        begin
+            CLK = 0;
+            RST = 1;
+        end
 
-	end
+        initial 
+        begin
+            #20 RST = 0;
+            for (count1 = 0; count1 < 9; count1 = count1 + 1) begin
+                #20 PE_IN = 10;
+                    PE_FILTER = 10;
+            end
+            for (count2 = 0; count2 < 9; count2 = count2 + 1) begin
+                #20 PE_IN = 5;
+                    PE_FILTER = 5;
+            end
+            for (count3 = 0; count3 < 9; count3 = count3 + 1) begin
+                #20 PE_IN = 2;
+                    PE_FILTER = 2;
+            end
+            for (count4 = 0; count4 < 9; count4 = count4 + 1) begin
+                #20 PE_IN = 20;
+                    PE_FILTER = 20;
+            end
+
+            #20 RST = 1;
+
+        end
 
 
-endmodule
+    endmodule **/
--- computation/pe.v
+++ computation/pe.v
@@ -1,21 +1,18 @@
-module pe (clk, rst, pe_in, pe_filter, pe_out, pe_vaild);
+module pe (clk, rst, pe_in, pe_filter, pe_out);
     input clk;
     input rst;
 
-    input [7 : 0] pe_in;
-    input [7 : 0] pe_filter;
-    reg acc_vaild_i;
+    input [7 : 0] pe_in; // pe에 들어오는 input값
+    input [7 : 0] pe_filter; // pe에 들어오는 filter값
     reg [7 : 0] pe_i;
     reg [7 : 0] pe_f;
 
-    output [7 : 0] pe_out;
-    output pe_vaild;
+    output [7 : 0] pe_out; //pe에서의 결과값
+    reg [7 : 0] pe_o;
 
-    // reg
+    // wire
     wire [7 : 0] mul_out;
     wire [7 : 0] acc_out;
-    reg [7 : 0] pe_o;
-    reg pe_vaild_o;
     wire count_9;
 
 
@@ -23,33 +20,20 @@ module pe (clk, rst, pe_in, pe_filter, pe_out, pe_vaild);
         if (rst == 1'b1) begin
             pe_i <= 8'b0;
             pe_f <= 8'b0;
-            acc_vaild_i <= 1'b0;
         end
         else begin
             pe_i <= pe_in;
             pe_f <= pe_filter;
-            acc_vaild_i <= 1'b1;
         end
     end
 
     eight_bit_multiplier_module eight_bit_multiplier( .a(pe_i), .b(pe_f), .out (mul_out));
-    accumulator accumulator_1( .clk(clk), .rst(rst), .acc_vaild(acc_vaild_i), .acc_in(mul_out), .acc_out(acc_out), .count_9(count_9));
-
+    accumulator accumulator_1( .clk(clk), .rst(rst), .acc_vaild(), .acc_in(mul_out), .acc_out(acc_out), .count_9(count_9));
 
     always @(*) begin
-        if (count_9 == 1'b1) begin
-            pe_o = acc_out;
-            pe_vaild_o = 1'b1;
-             acc_vaild_i = 1'b0;
-        end
-        else begin
-            pe_vaild_o = 1'b0;
-
-        end
+        pe_o = acc_out;
     end
 
     assign pe_out = pe_o;
-    assign pe_vaild = pe_vaild_o;
-
 
 endmodule
