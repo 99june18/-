1c8478a39aec2e255d7d78fc27c03fbfa23232a4
jinhyeok <jinhyeok1003@gmail.com>
2022-12-27 23:12:22 +0900

    single tb

--- KJH/systolic_array_2_by_2.v
+++ KJH/systolic_array_2_by_2.v
@@ -1,9 +1,9 @@
 
 
-module systolic_array_2_by_2 (clk, rst, active_sa2,a11,a12,a13,a14,a21,a22,a23,a24,a31,a32,a33,a34,a41,a42,a43,a44,
+module systolic_array_2_by_2 (clk, reset, active_sa2,a11,a12,a13,a14,a21,a22,a23,a24,a31,a32,a33,a34,a41,a42,a43,a44,
 b11,b12,b13,b21,b22,b23,b31,b32,b33,done_sa2,c11,c12,c21,c22);
 
-    input clk,rst,active_sa2;
+    input clk, reset, active_sa2;
     
     input [7:0] a11,a12,a13,a14,
                 a21,a22,a23,a24,
--- computation/single_process_array.v
+++ computation/single_process_array.v
@@ -33,6 +33,10 @@ b11,b12,b13,b21,b22,b23,b31,b32,b33,done_single,c11,c12,c21,c22);
               S28 = 28, S29 = 29, S30 = 30, S31 = 31, S32 = 32, S33 = 33, S34 = 34, S35 = 35, S36 = 36,
               S37 = 37;
 
+    pe u_pe(.clk(clk), .rst(rst), .pe_in(input_data), .pe_filter(input_filter),
+        .pe_out(pe_output), .mode_in(pe_mode), .mode_out());
+
+
     always @(posedge clk or posedge rst) 
     begin
         if(rst == 1'b1) 
@@ -329,7 +333,7 @@ b11,b12,b13,b21,b22,b23,b31,b32,b33,done_single,c11,c12,c21,c22);
                     input_data = 8'b0;
                     input_filter = 8'b0;
 
-                    next_state = 0;
+                    next_state <= S0;
 
                     done_single_w = 1'b1;
                 end
